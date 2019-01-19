ff73d6cb2d0b2d994b190c344107971c3831ce6e
adriangut <gut.adrian.95@gmail.com>
2019-01-19 13:59:13 +0100

    add youtube-video-viewer code

---
+++ youtube-video-viewer/src/components/video_list_item.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const VideoListItem = ({video, onVideoSelect}) => { //ES6, tells that passed var has a video and thus we want to make a new var video = to this one
+    const imageUrl = video.snippet.thumbnails.default.url;
+    
+    return( 
+        <li onClick={() => onVideoSelect(video)} className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageUrl} />
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">{video.snippet.title}</div>
+                </div>
+            </div>
+        </li>
+    );
+};
+
+export default VideoListItem;
